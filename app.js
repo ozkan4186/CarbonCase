0256a06faa0714ec85ddf91a8c7a4da743c18e01
özkan demir <ozkanndemir2009@gmail.com>
2023-10-23 00:23:46 +0300

    card structure finished

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const slider = document.querySelector(".slider");
 const filters = document.querySelectorAll(".filter");
 const modal = document.getElementById("modal");
 const closeButton = document.getElementById("close-button");
-const pro =document.getElementById("pro")
-const deatil=document.getElementById("detail")
-const showButton1 = document.getElementById("showContent1");
-const showButton2 = document.getElementById("showContent2");
-const content1 = document.getElementById("content1");
-const content2 = document.getElementById("content2");
+const pro = document.getElementById("pro");
+const deatil = document.getElementById("detail");
 
-// FİLTER 
+const card = document.querySelector(".card");
+
+// FİLTER
 filters.forEach((filter) => {
   filter.addEventListener("click", () => {
     filters.forEach((f) => f.classList.remove("active"));
@@ -34,7 +32,7 @@ function isElementInViewport(el) {
   );
 }
 
-//  MODAL  
+//  MODAL
 filters.forEach((filter) => {
   filter.addEventListener("click", () => {
     modal.style.display = "block";
@@ -50,23 +48,22 @@ window.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
-// PROPERTY 
+// PROPERTY
 
-
-
-
-
+const showButton1 = document.getElementById("showButton1");
+const showButton2 = document.getElementById("showButton2");
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
 
 showButton1.addEventListener("click", () => {
-  content1.style.display = "block"; // İçerik 1'i göster
-  content2.style.display = "none"; // İçerik 2'yi gizle
+  content1.style.display = "block";
+  content2.style.display = "none";
 });
 
 showButton2.addEventListener("click", () => {
-  content1.style.display = "none"; // İçerik 1'i gizle
-  content2.style.display = "block"; // İçerik 2'yi göster
+  content1.style.display = "none";
+  content2.style.display = "block";
 });
-
 
 const down1 = document.querySelector(".down1");
 const down2 = document.querySelector(".down2");
@@ -89,34 +86,15 @@ down2.addEventListener("click", function () {
   accordionList.style.display = "none";
   document.body.style.overflowX = "auto"; // Bu satır yatay kaydırmayı tekrar açar.
 });
-close.addEventListener("click",()=>{
+close.addEventListener("click", () => {
   down1.style.display = "block";
   down2.style.display = "none";
   up.style.display = "none";
   accordionList.style.display = "none";
   document.body.style.overflowX = "auto"; // B
-})
+});
 const clickableElement = document.getElementById("clickableElement");
 
 clickableElement.addEventListener("click", function () {
   clickableElement.style.borderBottom = "1px dashed "; // Tıklandığında kesik çizgiyi görünür kılar
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
