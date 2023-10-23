2a7e0d5bcb981d654f06cc824b6f9945ef71ec12
özkan demir <ozkanndemir2009@gmail.com>
2023-10-23 04:51:24 +0300

    last fixed

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const modal = document.getElementById("modal");
 const closeButton = document.getElementById("close-button");
 const pro = document.getElementById("pro");
 const deatil = document.getElementById("detail");
-
+const aramaInput = document.getElementById("aramaInput");
+const kartlar = document.querySelectorAll(".content1 .card");
 const card = document.querySelector(".card");
+const showButton1 = document.getElementById("showButton1");
+const showButton2 = document.getElementById("showButton2");
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
 
 // FİLTER
 filters.forEach((filter) => {
@@ -50,37 +55,32 @@ window.addEventListener("click", (e) => {
 });
 // PROPERTY
 
-const showButton1 = document.getElementById("showButton1");
-const showButton2 = document.getElementById("showButton2");
-const content1 = document.getElementById("content1");
-const content2 = document.getElementById("content2");
-
 showButton1.addEventListener("click", () => {
   content1.style.display = "block";
   content2.style.display = "none";
   showButton1.style.backgroundColor = "blue";
-  showButton2.style.backgroundColor = "gray"; // Doğru yazım
+  showButton2.style.backgroundColor = "gray";
 });
 
 showButton2.addEventListener("click", () => {
   content1.style.display = "none";
   content2.style.display = "block";
-  showButton2.style.backgroundColor = "blue"
-  showButton1.style.backgroundColor = "gray"; // Doğru 
+  showButton2.style.backgroundColor = "blue";
+  showButton1.style.backgroundColor = "gray";
 });
 
 const down1 = document.querySelector(".down1");
 const down2 = document.querySelector(".down2");
 const up = document.querySelector(".up");
 const accordionList = document.querySelector(".accordion-list");
-const close = document.querySelector(".close"); // close sınıfına sahip bir öğe olduğundan "." kullanmalısınız.
+const close = document.querySelector(".close");
 
 down1.addEventListener("click", function () {
   down1.style.display = "none";
   down2.style.display = "block";
   up.style.display = "block";
   accordionList.style.display = "block";
-  document.body.style.overflowX = "hidden"; // Bu satır yatay kaydırmayı kapatır.
+  document.body.style.overflowX = "hidden";
 });
 
 down2.addEventListener("click", function () {
@@ -88,39 +88,35 @@ down2.addEventListener("click", function () {
   down2.style.display = "none";
   up.style.display = "none";
   accordionList.style.display = "none";
-  document.body.style.overflowX = "auto"; // Bu satır yatay kaydırmayı tekrar açar.
+  document.body.style.overflowX = "auto";
 });
 close.addEventListener("click", () => {
   down1.style.display = "block";
   down2.style.display = "none";
   up.style.display = "none";
   accordionList.style.display = "none";
-  document.body.style.overflowX = "auto"; // B
+  document.body.style.overflowX = "auto";
 });
 const clickableElement = document.getElementById("clickableElement");
 
 clickableElement.addEventListener("click", function () {
-  clickableElement.style.borderBottom = "1px dashed "; // Tıklandığında kesik çizgiyi görünür kılar
+  clickableElement.style.borderBottom = "1px dashed ";
 });
 
-// Input alanını ve kartları seçin
-const aramaInput = document.getElementById("aramaInput");
-const kartlar = document.querySelectorAll(".content1 .card");
-
-// Input'a bir "input" olay dinleyicisi ekleyin
 aramaInput.addEventListener("input", kartlariFiltrele);
 
 function kartlariFiltrele() {
-  // Kullanıcının girdisini alın ve küçük harfe çevirin
   const aramaKelimesi = aramaInput.value.toLowerCase();
 
-  // Kartları gizle veya göster
   kartlar.forEach((kart) => {
     const kartMetni = kart.textContent.toLowerCase();
-    if (kartMetni.includes(aramaKelimesi) && (aramaKelimesi.length === 2 || aramaKelimesi.length === 3)) {
-      kart.style.display = "block"; // Eşleşen kartları göster
+    if (
+      kartMetni.includes(aramaKelimesi) &&
+      (aramaKelimesi.length === 2 || aramaKelimesi.length === 3)
+    ) {
+      kart.style.display = "block";
     } else {
-      kart.style.display = "none"; // Eşleşmeyen kartları gizle
+      kart.style.display = "none";
     }
   });
 }
